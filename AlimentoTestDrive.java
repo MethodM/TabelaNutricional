f6e65a9dd21ac5e237caf138727d20d6d9de0cc9
MethodM <casagrande.theus@gmail.com>
2025-01-05 23:31:21 -0300

    AlimentoTestDrive(Classe Teste)

--- AlimentoTestDrive.java
+++ AlimentoTestDrive.java
@@ -1,7 +1,17 @@
+import java.sql.SQLOutput;
+
 public class AlimentoTestDrive {
     public static void main(String [] args) {
-        Alimento maca = new Alimento(67.60, 0.08, 17.95, 0.34);
-        Alimento carne = new Alimento(287, 19.05, 0, 26.41);
-    }
+        Alimento maca = ;
+        // Alimento carne = new Alimento("Carne", 287, 19.05, 0, 26.41);
 
+        //Lista de Alimento
+        ListaDeAlimentos listaDeAlimentos = new ListaDeAlimentos();
+        listaDeAlimentos.addAlimento(new Alimento("Maçã", 67.60, 0.08, 17.95, 0.34));
+        listaDeAlimentos.addAlimento(new Alimento (carne);
+
+
+        System.out.println("Maçã: " + maca.getCaloriasTotais() + " calorias");
+        System.out.println("Carne: " + carne.getCaloriasTotais()+ " calorias");
+    }
 }
