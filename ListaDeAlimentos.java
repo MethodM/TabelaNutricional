8d93c8633155a272b70d40be8358b766a0de4553
MethodM <casagrande.theus@gmail.com>
2025-01-06 00:10:26 -0300

    Lista de Alimentos.

--- ListaDeAlimentos.java
+++ ListaDeAlimentos.java
@@ -5,6 +5,12 @@ public class ListaDeAlimentos {
     // Lista uma coleção de objetos da classe Alimento
     List<Alimento> listaDeAlimentos = new ArrayList<>();
 
+    public void listarAlimentos() {
+        for (Alimento alimento : listaDeAlimentos) {
+            System.out.println("Nome: " + alimento.getNome() + " Calorias: " + alimento.getCalorias());
+
+        }
+    }
 
     // Método de adicionar alimentos:
     public void addAlimento(Alimento Alimento) {
