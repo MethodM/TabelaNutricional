4b5d07cae80ce14fd99f66edc2647715e876e49a
MethodM <casagrande.theus@gmail.com>
2025-01-07 04:17:53 -0300

    Classe Usuário.

---
+++ Usuario.java
@@ -0,0 +1,7 @@
+public class Usuario {
+    // Método Scanner -> Associar a ele uma lista de alimentos consumido
+
+    // Somar a quantidade total dos alimentos
+
+    // Somar a quantidade total de calorias ingeridas
+}
