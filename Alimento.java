315a78f477f07d08c68e40402d599b38933c1238
MethodM <casagrande.theus@gmail.com>
2025-01-07 04:17:15 -0300

    Classe Principal Alimentos.

--- Alimento.java
+++ Alimento.java
@@ -12,15 +12,13 @@ public class Alimento {
     }
     // Retirado todos os valores "static" dos atributos, para não compartilhar entre todas as instâncias da classe Alimento
 
-
-    //********* Passar valores como Parâmetros
-    // para aceitar alterações futuras
-    public void adicionarAlimento(double calorias, double gorduras, double carboidratos, double proteina) {
+    //********* Passar valores como Parâmetro para aceitar alterações futuras
+    public void adicionarAlimento(double calorias, double gorduras, double carboidratos, double proteina, double quantidade) {
         this.calorias = calorias;
         this.gorduras = gorduras;
         this.carboidratos = carboidratos;
         this.proteina = proteina;
-        // this.quantidade = quantidade?
+        this.quantidade = quantidade;
 
     }
     public static void armazenarAlimento () {
@@ -76,8 +74,8 @@ public class Alimento {
     }
     // Método que Calcula todas as Calorias, com os valors de 1g carbs = 4 calorias, 1g proteina = 4 calorias e 1g de gorduras = 9 calorias
     public double getCaloriasTotais() {
-        double getCaloriasTotais = (double) (carboidratos * 4 + proteina * 4 + gorduras * 9);
+        double getCaloriasTotais = (carboidratos * 4 + proteina * 4 + gorduras * 9);
         return getCaloriasTotais; // Retorna o valor do método após o cálculo
+        // return (carboidratos * 4) + (proteina * 4) + (gorduras * 9);
     }
-
 }
